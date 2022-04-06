6cc8d12969d99ebf44a3572581a33592948cecba
Luis Cubillos <lhcubill@umich.edu>
2022-04-06 15:20:54 -0400

    tested on QTM computer

--- latency/xbee_latency_check.py
+++ latency/xbee_latency_check.py
@@ -8,12 +8,12 @@ import serial
 import numpy
 from timeit import default_timer as getTime
 
-SERIAL_PORT = "COM7"
-ITERATION_COUNT = 1000
+SERIAL_PORT = "COM6"
+ITERATION_COUNT = 10000
 MSG_LENGTH = 40
 
 try:
-    sconn = XBeeSerialPort(230400, SERIAL_PORT, timeout=1)
+    sconn = XBeeSerialPort(230400, SERIAL_PORT, timeout=0.1)
     sconn.open()
 except serial.SerialException as e:
     print("Check SERIAL_PORT variable is correct for device being tested.")
@@ -27,6 +27,7 @@ while sconn.readline():
 # and receive the serial reply.
 init_time = getTime()
 results = numpy.zeros(ITERATION_COUNT, dtype=numpy.float64)
+num_timeouts = 0
 for i in range(ITERATION_COUNT):
     tx_time = getTime()
     msg = b"A" * MSG_LENGTH + b"."
@@ -36,7 +37,10 @@ for i in range(ITERATION_COUNT):
     if r:
         results[i] = rx_time - tx_time
     else:
-        raise RuntimeError("Serial RX Timeout.")
+        print(r)
+        print(i)
+        num_timeouts +=1
+        results[i] = numpy.nan
 
 total_time = getTime() - init_time
 sconn.close()
@@ -47,10 +51,11 @@ results = results * 1000.0
 print("Results")
 print(f"\tMsg length: {MSG_LENGTH}")
 print("\tCount: {}".format(results.shape))
-print("\tAverage: {:.3f} msec".format(results.mean()))
-print("\tMedian: {:.3f} msec".format(numpy.median(results)))
-print("\tMin: {:.3f} msec".format(results.min()))
-print("\tMax: {:.3f} msec".format(results.max()))
-print("\tStdev: {:.3f} msec".format(results.std()))
+print("\tAverage: {:.3f} msec".format(numpy.nanmean(results)))
+print("\tMedian: {:.3f} msec".format(numpy.nanmedian(results)))
+print("\tMin: {:.3f} msec".format(numpy.nanmin(results)))
+print("\tMax: {:.3f} msec".format(numpy.nanmax(results)))
+print("\tStdev: {:.3f} msec".format(numpy.nanstd(results)))
 print(f"\tTotal time: {total_time:.3f} s")
 print(f"\tAvg frequency: {results.shape[0] / total_time:.2f} Hz")
+print(f"\tTimeouts: {num_timeouts}")
